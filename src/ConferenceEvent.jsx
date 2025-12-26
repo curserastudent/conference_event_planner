17c7a2708a1509855404a293419918ef7f6660a8
curserastudent <mbustama@uoc.edu>
2025-12-25 21:51:21 -0500

    Button error

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -154,16 +154,16 @@ const mealsTotalCost = calculateTotalCost("meals");
     return (
         <>
             <navbar className="navbar_event_conference">
-                <div className="company_logo">Conference Expense Planner</div>
+                <div className="company_logo">Conference</div>
                 <div className="left_navbar">
                     <div className="nav_links">
                         <a href="#venue" onClick={() => navigateToProducts("#venue")} >Venue</a>
                         <a href="#addons" onClick={() => navigateToProducts('#addons')}>Add-ons</a>
                         <a href="#meals" onClick={() => navigateToProducts('#meals')}>Meals</a>
+                        <button className="details_button" onClick={() => setShowItems(!showItems)}>
+                            Show Details
+                        </button>
                     </div>
-                    <button className="details_button" onClick={() => setShowItems(!showItems)}>
-                        Show Details
-                    </button>
                 </div>
             </navbar>
             <div className="main_container">
